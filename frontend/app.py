e3fed04b1dc9fba268c0f7a290191e8fb919b751
Yutao Xu <99435308+yutao-xu@users.noreply.github.com>
2025-09-06 19:28:41 -0400

    Add sample open door

--- frontend/app.py
+++ frontend/app.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from open_door import open_door
 
 def pickup_handler():
     print("picked up")
@@ -11,8 +12,12 @@ root = tk.Tk()
 root.title("Birdie Fix")
 root.geometry("1920x1080")
 
-pickup_button = tk.Button(root, text="Pickup", width=25, command=pickup_handler)
+pickup_button = tk.Button(root, text="Open", width=25, command=pickup_handler)
 dropoff_button = tk.Button(root, text="Drop Off", width=25, command=dropoff_handler)
 pickup_button.pack()
 dropoff_button.pack()
+
+# sample for test
+open_button = tk.Button(root, text="Open", width=25, command=open_door)
+open_button.pack()
 root.mainloop()
---
+++ frontend/open_door.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+import time
+
+def open_door():
+    RELAY_PIN = 12
+
+    GPIO.setmode(GPIO.BCM)
+
+    GPIO.setwarnings(False)
+
+    GPIO.setup(RELAY_PIN, GPIO.OUT)
+
+    GPIO.output(RELAY_PIN, GPIO.HIGH)
+    time.sleep(1)
+
+    GPIO.output(RELAY_PIN, GPIO.LOW)
+    time.sleep(1)
