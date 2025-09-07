76810638122a1959f83d34f6583c38e88eae3e41
Yutao Xu <99435308+yutao-xu@users.noreply.github.com>
2025-09-07 00:56:58 -0400

    Add frames

--- frontend/app.py
+++ frontend/app.py
@@ -1,23 +1,83 @@
 import tkinter as tk
+from tkinter import ttk
 from open_door import open_door
 
-def pickup_handler():
-    print("picked up")
+class App(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs,)
+        self.title("Birdie Fix")
+        self.geometry("1280x720")
 
-def dropoff_handler():
-    print("dropped off")
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
 
-root = tk.Tk()
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
 
-root.title("Birdie Fix")
-root.geometry("1920x1080")
+        self.frames = {}
 
-pickup_button = tk.Button(root, text="Pick Up", height=5, width=20, command=pickup_handler)
-dropoff_button = tk.Button(root, text="Drop Off", height=5, width=20, command=dropoff_handler)
-pickup_button.pack()
-dropoff_button.pack()
+        for F in (Homepage, ChooseTension, ChooseString, EnterNumber):
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+        
+        self.show_frame(Homepage)
+    
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
 
-# sample for test
-open_button = tk.Button(root, text="Open", height=5, width=20, command=open_door)
-open_button.pack()
-root.mainloop()
+class Homepage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        
+        label = ttk.Label(self, text="Birdie Fix")
+        label.grid(row=0, column=4, padx=10, pady=10)
+
+        dropoff_button = ttk.Button(self, text="Pickup", command=lambda: controller.show_frame(ChooseTension))
+        dropoff_button.grid(row=1, column=4, padx=10, pady=10)
+
+        pickup_button = ttk.Button(self, text="String New Racket", command=lambda: controller.show_frame(ChooseTension))
+        pickup_button.grid(row=1, column=8, padx=10, pady=10)
+
+class ChooseTension(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        
+        label = ttk.Label(self, text="Choose a Tension")
+        label.grid(row=0, column=4, padx=10, pady=10)
+
+        back_button = ttk.Button(self, text="Back", command=lambda: controller.show_frame(Homepage))
+        back_button.grid(row=1, column=4, padx=10, pady=10)
+
+        next_button = ttk.Button(self, text="Next", command=lambda: controller.show_frame(ChooseString))
+        next_button.grid(row=2, column=4, padx=10, pady=10)
+
+class ChooseString(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        
+        label = ttk.Label(self, text="Choose a String")
+        label.grid(row=0, column=4, padx=10, pady=10)
+
+        back_button = ttk.Button(self, text="Back", command=lambda: controller.show_frame(ChooseTension))
+        back_button.grid(row=1, column=4, padx=10, pady=10)
+
+        next_button = ttk.Button(self, text="Next", command=lambda: controller.show_frame(EnterNumber))
+        next_button.grid(row=2, column=4, padx=10, pady=10)
+
+class EnterNumber(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        
+        label = ttk.Label(self, text="Enter Your Phone Number")
+        label.grid(row=0, column=4, padx=10, pady=10)
+
+        back_button = ttk.Button(self, text="Back", command=lambda: controller.show_frame(ChooseString))
+        back_button.grid(row=1, column=4, padx=10, pady=10)
+
+        open_button = ttk.Button(self, text="Open Door", command=open_door)
+        open_button.grid(row=2, column=4, padx=10, pady=10)
+
+app = App()
+app.mainloop()
