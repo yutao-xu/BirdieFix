0194689c2e1a7c689f2658f200e60ff6d0806c54
Yutao Xu <99435308+yutao-xu@users.noreply.github.com>
2025-09-09 02:42:05 -0400

    Fix backend structure

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,13 +1,13 @@
-package com.example.birdiefix;
+package com.example.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BirdiefixApplication {
+public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BirdiefixApplication.class, args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
--- backend/src/test/java/com/example/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/example/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.birdiefix;
+package com.example.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BirdiefixApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
