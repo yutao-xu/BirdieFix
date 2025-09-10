84949068642a882626d793ff9890a577356f7b05
Yutao Xu <99435308+yutao-xu@users.noreply.github.com>
2025-09-10 02:46:23 -0400

    Change API and add DTO

--- backend/src/main/java/com/example/backend/DropoffController.java
+++ backend/src/main/java/com/example/backend/DropoffController.java
@@ -1,5 +1,6 @@
 package com.example.backend;
 
+import com.example.backend.model.RacketDTO;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DropoffController {
 
-    @PostMapping(path = "/dropoff", consumes = "application/json", produces = "application/json")
-    public String Dropoff(@RequestBody Order order) {
-        return "Tension: " + order.getTension() +
-                "\n" + "String: " + order.getRacketString() +
-                "\n" + "Phone Number: " + order.getPhoneNumber();
+    @PostMapping(path = "/api/dropoff", consumes = "application/json", produces = "application/json")
+    public String Dropoff(@RequestBody RacketDTO racketDTO) {
+        return "Tension: " + racketDTO.tension() +
+                "\n" + "String: " + racketDTO.racketString() +
+                "\n" + "Phone Number: " + racketDTO.phoneNumber();
     }
 }
---
+++ backend/src/main/java/com/example/backend/model/RacketDTO.java
@@ -0,0 +1,3 @@
+package com.example.backend.model;
+
+public record RacketDTO(int tension, String racketString, String phoneNumber) {}
